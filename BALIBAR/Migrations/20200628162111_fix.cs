05df6eeffbd5132e749a947e6fca523bf0e5aff7
edenpd <eden1998p@gmail.com>
2020-06-28 22:48:35 +0300

    DB fix attempt

---
+++ BALIBAR/Migrations/20200628162111_fix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BALIBAR.Migrations
+{
+    public partial class fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Bar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Bar",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
