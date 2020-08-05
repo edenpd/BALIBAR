66d8f19367effe1ea4c0c39097f2f923e2aeb8a8
Tomer Erusalimsky <bboypsyfly@gmail.com>
2020-08-05 22:11:04 +0300

    Resersvation: Authorization, search design, button color fixes and selection of resrvations according to the logged in customer(if he isn't admin).

--- BALIBAR/Controllers/ReservationsController.cs
+++ BALIBAR/Controllers/ReservationsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using BALIBAR.Data;
 using BALIBAR.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BALIBAR.Controllers
 {
@@ -18,42 +19,57 @@ namespace BALIBAR.Controllers
         private readonly BALIBARContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
+        // Constructors
         public ReservationsController(BALIBARContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
 
+        // Methods
         // GET: Reservations
+        [Authorize]
         public async Task<IActionResult> Index(string barName, DateTime fromDate, DateTime toDate)
         {
-            IQueryable<Reservation> students = _context.Reservation.Include(r => r.Bar);
+            IQueryable<Reservation> reservations = _context.Reservation.Include(r => r.Bar);
 
             // Build the where clause according to the filter parameters:
             // Check is the barname filter was filled
             if (!String.IsNullOrEmpty(barName))
             {
-                students = students.Where(r => r.Bar.Name.Contains(barName));
+                reservations = reservations.Where(r => r.Bar.Name.Contains(barName));
             }
             
             // Check if the fromDate filter was filled
             if (fromDate != DateTime.MinValue)
             {
-                students = students.Where(r => r.DateTime.Date >= fromDate.Date);
+                reservations = reservations.Where(r => r.DateTime.Date >= fromDate.Date);
             }
 
             // Check if the toDate filter was filled
             if (toDate != DateTime.MinValue)
             {
-                students = students.Where(r => r.DateTime.Date <= toDate);
+                reservations = reservations.Where(r => r.DateTime.Date <= toDate);
             }
 
-            List<Reservation> list = await students.ToListAsync();
+            // Check if the user isn't an admin.
+            if (!User.IsInRole("Admin"))
+            {
+
+                // Get the connected user.
+                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+                // Get only reservations that belong to the connected user.
+                reservations = reservations.Where(r => r.CustomerId == user.Id);
+            }
+
+            List<Reservation> list = await reservations.ToListAsync();
 
             return View(list);
         }
 
         // GET: Reservations/Details/5
+        [Authorize]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -72,6 +88,7 @@ namespace BALIBAR.Controllers
         }
 
         // GET: Reservations/Create
+        [Authorize]
         public IActionResult Create(string? barId)
         {
             // Check if the id field is filled, and if so, hide the bar field.
@@ -88,6 +105,7 @@ namespace BALIBAR.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Id,DateTime,AttendeesNum,BarID")] Reservation reservation)
         {
             if (ModelState.IsValid)
@@ -108,6 +126,7 @@ namespace BALIBAR.Controllers
         }
 
         // GET: Reservations/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -128,6 +147,7 @@ namespace BALIBAR.Controllers
         // POST: Reservations/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,DateTime,AttendeesNum,BarID")] Reservation reservation)
@@ -161,6 +181,7 @@ namespace BALIBAR.Controllers
         }
 
         // GET: Reservations/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -181,6 +202,7 @@ namespace BALIBAR.Controllers
         // POST: Reservations/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var reservation = await _context.Reservation.FindAsync(id);
